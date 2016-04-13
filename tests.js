a5ea610baefced29de4ea46324e9884e151653fe
alexedev <work2alex@gmail.com>
2016-04-14 01:12:50 +0200

    QUnit tests of functionality, DOM test to be added

---
+++ tests.js
@@ -0,0 +1,43 @@
+// I did not add any DOM tests, only functional 
+
+QUnit.test( "add", function() {
+  equal( add(2,6), 8, "2 + 6 = 8" );
+});
+QUnit.test( "multiply", function() {
+  equal( multiply(2,6), 12, "2 * 6 = 12" );
+});
+QUnit.test( "divide", function() {
+  equal( divide(12,6), 2, "12 / 6 = 2" );
+});
+QUnit.test( "substract", function() {
+  equal( substract(33,11), 22, "33 - 22 = 11" );
+});
+QUnit.test( "calculate result", function() {
+    // for now this test works only if all available instruction files are required
+    // need to be refactored to be flexible
+  var instructionsArray = [
+      {"operator" : "apply", "number" : 3},
+      {"operator" : "multiply", "number" : 8},
+      {"operator" : "apply", "number" : 9},
+      {"operator" : "apply", "number" : 5},
+      {"operator" : "divide", "number" : 4},
+      {"operator" : "substract", "number" : 4}
+      // here we have 5 * 8 / 4 - 4 = 6
+  ]
+  equal(calculateResult(instructionsArray), 6, "The result is calculated: 5 * 8 / 4 - 4 = 6" );
+  //the next line removes the side effect of the test (the result is printed to the page)
+  document.getElementById("result").innerHTML = "N/A";
+});
+
+QUnit.test( "without the Apply instruction", function() {
+  var instructionsArray = [
+      {"operator" : "multiply", "number" : 8},
+      {"operator" : "divide", "number" : 4},
+      {"operator" : "substract", "number" : 4}
+  ]
+  equal(calculateResult(instructionsArray), undefined, "Apply operator was not added, function returns undefined" );
+  //the next line removes the side effect of the test (warning appends to the form)
+  document.querySelector("form").removeChild(document.getElementById("warning"));
+});
+
+
