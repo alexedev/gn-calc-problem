5d53fe5e52e25c0399769e656bdba03a13093507
alexedev <work2alex@gmail.com>
2016-04-14 02:07:13 +0200

    added comments and cosmetic changes

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ const btnAdd = document.getElementById("btnAdd");
 const resultOutput = document.getElementById("result");
 const selector = document.getElementById("operators");
 const instructions = document.getElementById("instructions");
+const form = document.querySelector("form");
+let warning;
+let instrArray = []; // populated with instructions, used in calculation
+// "Apply" instruction is added by default
 const optionApply = new Option("Apply", "apply", false, false);
-let form = document.querySelector("form");
-let instrArray = [];
 selector.appendChild(optionApply);
 
-var addInstruction = () => {
-	let operator = selector.options[selector.selectedIndex].value;
-	console.log(operator);
-	let operatorText = selector.options[selector.selectedIndex].text;
-	let inputNumber = document.querySelector("input").value;
+//Adds instruction to DOM (list) and to an array used for final calculation
+var addInstruction = (operator,operatorText,inputNumber) => {
 	if (inputNumber) {
 		console.log("Adding new instruction to the list and instrArray: " + operatorText + " " + inputNumber + " ...");
-		instrArray.push({operator:operator,number:parseInt(inputNumber)});
+		instrArray.push({operator:operator,number:inputNumber});
+        // next add it to DOM
 		let liNode = document.createElement("li")
 		liNode.innerHTML = operatorText + " " + inputNumber;
         let oldApplyLi; // variable to store li for Apply instruction
@@ -54,9 +54,9 @@ var addInstruction = () => {
 	}
 }
 
+// the function calculates result, input is an array of instructions
 var calculateResult = (instrs) => {
     //if there is no Apply operator - show Warning to the user and terminate the function
-    let warning;
     if (!instrs.find((instr) => (instr.operator === "apply"))) {
         if (!document.getElementById("warning")) {
             warning = document.createElement("div");
@@ -67,51 +67,66 @@ var calculateResult = (instrs) => {
         }
         return;
     }
-	let arrayCopy1 = instrs;
-	let arrayCopy2 = instrs;
-    // only the last "apply" is taken into account
+    // get the Apply value (only the last "apply" is taken into account)
 	let applyNumber = instrs.filter((instr)=>(instr.operator=="apply")).pop().number;
     // get array of instructions without "apply"s
 	let instrWithoutApply = instrs.filter((instr)=>(instr.operator!="apply"));
-	let result = applyNumber;
-	console.log(applyNumber);
-	console.log(instrWithoutApply);
-    // apply all the instructions
+	// apply all the instructions
+    let result = applyNumber;
 	for (var i of instrWithoutApply) {
 		console.log(i);
         //not the best idea to use window["functionname"] to call instructions, needs to be refactored
 		result = window[i.operator](result, i.number);
-		console.log(result);
 	}
+    // add result to DOM instead of N/A
 	document.getElementById("result").innerHTML = result;
     console.log("Result is calculated");
-    if (document.getElementById("warning")) {
-        warning = document.getElementById("warning")
-        form.removeChild(warning);
-    }
+    // remove "no-Apply" warning if presented
+    removeWarning();
     return result;
 }
 
-let resetCalc = () => {
+const removeWarning = () => {
+    if (document.getElementById("warning")) {
+        warning = document.getElementById("warning");
+        form.removeChild(warning);
+    }
+}
+
+// resets calculator
+const resetCalc = () => {
     instrArray = [];
 	instructions.innerHTML = "";
 	document.querySelector("input").value = "";
     document.getElementById("result").innerHTML = "N/A";
+    removeWarning();
 	console.log("Calculator is reset");
 }
 
+
+// Event listeners for buttons
+
+// Add
 btnAdd.addEventListener("click", (e) => {
+    // get input for a new instruction from DOM 
+    let oper = selector.options[selector.selectedIndex].value;
+	let operText = selector.options[selector.selectedIndex].text;
+	let inputNum = parseInt(document.querySelector("input").value);
 	e.preventDefault();
-	addInstruction();
+    // add the instruction
+	addInstruction(oper,operText,inputNum);
 	e.stopPropagation();
 });
 
+// Calculate
 btnCalculate.addEventListener("click", (e) => {
 	e.preventDefault();
+    // istrArray contains all added instructions
 	calculateResult(instrArray);
     e.stopPropagation();
 });
 
+// Reset
 btnReset.addEventListener("click", (e) => {
 	e.preventDefault();
     resetCalc();
